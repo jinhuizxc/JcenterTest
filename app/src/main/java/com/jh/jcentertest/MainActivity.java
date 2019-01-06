9ea3c86582ab57f90c992a2352bd217f128bdaf8
jinhuizxc <1004260403@qq.com>
2019-01-06 16:07:18 +0800

    Initial commit

---
+++ app/src/main/java/com/jh/jcentertest/MainActivity.java
@@ -0,0 +1,19 @@
+package com.jh.jcentertest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ *
+ * Android Studio发布项目到jcenter,一行代码引入Module
+ * https://www.cnblogs.com/yishaochu/p/7495703.html
+ *
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
